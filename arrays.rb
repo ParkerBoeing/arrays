dabc472ed829a17d7c699f7000d15bdabeb157c9
Parker Boeing <parker.boeing@gmail.com>
2023-04-12 14:15:40 -0600

    Demonstrate 4 different array methods

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,11 @@ friends = ["Erin", "Ethan", "Ken", "Laura"]
 ages = [28, 31, 57, 43]
 climbing_grades [5.12, 5.10, 5.13, 5.11]
 sends [true, false, false, true]
+# The code below will add the name Raphael to the end of the array stored in friends
+friends.push("Raphael")
+# The code below will remove the final value (true) from the sends array
+sends.pop()
+# The code below will remove the first 2 elements (28, 31) from the ages array
+ages.shift(2)
+# The code below will add a new element (5.14) to the beginning of the climbing_grades array
+climbing_grades.unshift(5.14)
